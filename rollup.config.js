ed48942447f215c0eb48a60aaaf46e13d41a0258
Thitav <thiptav@gmail.com>
2025-02-03 18:13:54 -0300

    Fixed packaging

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
 import typescript from "@rollup/plugin-typescript";
 
-const modules = ["app", "fs", "emulator", "ui", "project", "resource"];
+const modules = [
+  "app",
+  "fs",
+  "emulator",
+  "ui",
+  "project",
+  "resource",
+  "extension",
+];
 
 export default {
   input: Object.fromEntries(modules.map((name) => [name, `src/${name}.ts`])),
